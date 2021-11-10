01e2b5d00d427e295d5ed8ef1b24c28bf9addeeb
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-10 22:57:46 +0330

    Update logout

--- lib/app/home_page/home_page.dart
+++ lib/app/home_page/home_page.dart
@@ -1,11 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  HomePage({@required this.onSignOut});
+  final VoidCallback onSignOut;
+
+  Future<void> _signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      onSignOut();
+    }
+    catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: _signOut,
+            child: Text(
+              'Logout',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+        ],
         elevation: 10.0,
       ),
       body: _buildContent(),
