91c375208bb2efa9d4745991f130e2109f3ac12e
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-10 20:49:33 +0330

    Add landing page

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:time_tracker_flutter_cource/app/sign_in/sign_in_page.dart';
+import 'package:time_tracker_flutter_cource/app/landing_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Time Tracker',
+      // title: 'Time Tracker',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: SignInPage(),
+      home: LandingPage(),
     );
   }
 }
